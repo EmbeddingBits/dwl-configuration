2eb1f9ca0cbf97e01cff777b4f89c9c74eca61cb
BitsExploited <tspamiitesh@gmail.com>
2025-06-04 20:15:27 +0530

    Adding all the required files

---
+++ someblocks/blocks.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/    /*Command*/                  /*Update Interval*/    /*Update Signal*/
+    {"  ", "cat /sys/class/power_supply/BAT0/capacity | sed 's/$/%/'", 5,      0},
+    {"  ", "pamixer --get-volume | sed 's/$/%/'",                   5,      0},
+    {"󱑃  ", "date +%H:%M",                                           5,      0},
+    {"  ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",      30,     0},
+    {"  ", "sh ~/dwl-config/scripts/wifi.sh",                       5,      0},
+    
+    /* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
+    /* {"", "date '+%b %d (%a) %I:%M%p'",          0,      10}, */
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
