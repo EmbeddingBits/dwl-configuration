e5ce805f346cc9c1b3990112a5f182f4919c7b23
BitsExploited <tspamiitesh@gmail.com>
2025-06-05 14:03:24 +0530

    Adding all the required files

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/                  /*Update Interval*/    /*Update Signal*/
+    {"  ", "sh ~/dwl-configuration/scripts/playing.sh",             5,      0},
     {"  ", "cat /sys/class/power_supply/BAT0/capacity | sed 's/$/%/'", 5,      0},
     {"  ", "pamixer --get-volume | sed 's/$/%/'",                   1,      0},
     {"󱑃  ", "date +%H:%M",                                           5,      0},
     {"  ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",      30,     0},
-    {"  ", "sh ~/dwl-configuration/scripts/wifi.sh",                       5,      0},
+    {"  ", "sh ~/dwl-configuration/scripts/wifi.sh",                5,      0},
     
     /* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
     /* {"", "date '+%b %d (%a) %I:%M%p'",          0,      10}, */
