c6b965133bb4ca65e4d0b592a91f48347f5796b3
BitsExploited <tspamiitesh@gmail.com>
2025-06-05 21:34:00 +0530

    Adding all the required files

--- somebar/src/config.hpp
+++ somebar/src/config.hpp
@@ -10,16 +10,16 @@ constexpr int paddingX = 10;
 constexpr int paddingY = 3;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "CaskaydiaMono Nerd Font 12";
+constexpr const char* font = "JetBrainsMono Nerd Font Bold 12";
 
 constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
 constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x5e, 0x81, 0xac)};
 constexpr const char* termcmd[] = {"kitty", nullptr};
 
 static std::vector<std::string> tagNames = {
-	"1", "2", "3",
-	"4", "5", "6",
-	"7", "8", "9",
+	"  ", " 󰈹 ", "  ",
+	"  ", "  ", " 6 ",
+	" 7 ", " 8 ", " 9 ",
 };
 
 constexpr Button buttons[] = {
