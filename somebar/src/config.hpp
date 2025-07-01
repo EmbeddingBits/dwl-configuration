d200a65174ea36617add35f8002a4d4c90c63273
embeddingbits <tspamiitesh@gmail.com>
2025-07-01 06:40:14 +0530

    Small changes

--- somebar/src/config.hpp
+++ somebar/src/config.hpp
@@ -12,7 +12,7 @@ constexpr int paddingY = 3;
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
 constexpr const char* font = "JetBrainsMono Nerd Font Bold 12";
 
-constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
+constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x2e, 0x34, 0x40)};
 constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x5e, 0x81, 0xac)};
 constexpr const char* termcmd[] = {"kitty", nullptr};
 
